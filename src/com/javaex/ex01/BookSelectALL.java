5b53f8932ce397e1edf492129b7fdb112069d70e
minnnk <jubal9153@gmail.com>
2022-05-20 17:42:56 +0900

    2022-05-20

---
+++ src/com/javaex/ex01/BookSelectALL.java
@@ -0,0 +1,78 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookSelectALL {
+
+	public static void main(String[] args) {
+			// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			//SQL문 준비
+			String query = "";
+			query += " select  b.book_id ";
+			query += "         ,b.title ";
+			query += "         ,b.pubs ";
+			query += "         ,b.pub_date ";
+			query += "         ,a.author_id ";
+			query += "         ,a.author_name ";
+			query += "         ,a.author_desc ";
+			query += " from book b , author a ";
+			query += " where a.author_id = b.author_id ";
+		
+			
+			//바인딩
+			pstmt = conn.prepareStatement(query);
+			
+			//실행
+			rs = pstmt.executeQuery();
+			
+			// 4.결과처리
+			while(rs.next()) {
+				int bookId = rs.getInt(1);
+				String title = rs.getString(2);
+				String pubs = rs.getString(3);
+				String pubDate = rs.getString(4);
+				int authorId = rs.getInt(5);
+				String authorName = rs.getString(6);
+				String authorDesc = rs.getString(7);
+				System.out.println(bookId+" ,"+title+", "+pubs+", "+pubDate+", "+authorId+", "+authorName+", "+authorDesc);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+			try {
+			if (rs != null) {
+				rs.close();
+			} 
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+
+	}
+
+}
